51f4a8bff9fb985fb3bd1f03ed8c90a698bb3f44
jguerrero101314 <joelg1014@hotmail.com>
2020-02-13 13:00:47 -0500

    20. Introducción general a los tipos primitivos

---
+++ Sección 3 Fundamentos de JavaScript, primitivos, arreglos, objetos y funciones básicas/js/Datosprimitivos.js
@@ -0,0 +1,21 @@
+let nombre = 'Joel Guerrero';
+console.log(nombre);
+
+
+nombre = 'Superman';
+console.log(nombre);
+
+nombre = "Ruth";
+console.log(nombre);
+nombre = `nico`;
+console.log(nombre);
+
+console.log(typeof nombre);
+
+
+
+let esMarvel = true;
+console.log(typeof esMarvel);
+
+let edad = 22;
+console.log(typeof edad);
