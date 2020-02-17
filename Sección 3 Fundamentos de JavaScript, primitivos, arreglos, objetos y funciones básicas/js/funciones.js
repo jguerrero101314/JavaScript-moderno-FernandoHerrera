0cf9e942431c4fbd2a42ef846b2190fc8eeedbf9
jguerrero101314 <joelg1014@hotmail.com>
2020-02-17 12:34:08 -0500

    27. Retorno de las funciones

--- Sección 3 Fundamentos de JavaScript, primitivos, arreglos, objetos y funciones básicas/js/funciones.js
+++ Sección 3 Fundamentos de JavaScript, primitivos, arreglos, objetos y funciones básicas/js/funciones.js
@@ -3,6 +3,10 @@ function saludar(nombre) {
     console.log('hola: ' + nombre);
     console.log(arguments);
 
+    return 10; // si no tiene return regresa undefined
+    //esto nunca se va a ejecutar
+    console.log('Soy un codigo que esta despues del return');
+
 }
 const saludar2 = function(nombre) { // funcion anonima
     // console.log('hola mundo2');
@@ -16,7 +20,30 @@ const saludarFlecha2 = (nombre) => {
 }
 
 // saludar();
-saludar('Joel', 40, true, 'Pnama');
-saludar2('Alex');
-saludarFlecha();
-saludarFlecha2('Ruth');
+// saludar('Joel', 40, true, 'Pnama');
+// saludar2('Alex');
+// saludarFlecha();
+// saludarFlecha2('Ruth');
+const retornoSaludar = saludar('Joel', 40, true, 'Pnama');
+console.log(retornoSaludar);
+
+function sumar(a, b) {
+    return a + b;
+}
+const sumar2 = (a, b) => {
+    return a + b;
+}
+
+const sumar3 = (a, b) => a + b;
+console.log(sumar(1, 2));
+console.log(sumar2(1, 2));
+console.log(sumar3(2, 2));
+
+function getAleatorio() {
+    return Math.random();
+}
+
+const getAleatorio2 = () => Math.random();
+
+console.log('getAleatorio1 = ' + getAleatorio());
+console.log('getAleatorio2 = ' + getAleatorio2());
