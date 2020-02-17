3b8301093d7c99b47b58b6e2a0df13d74dd83a6a
jguerrero101314 <joelg1014@hotmail.com>
2020-02-17 11:48:13 -0500

    25. Más detalles sobre los objetos literales

--- Sección 3 Fundamentos de JavaScript, primitivos, arreglos, objetos y funciones básicas/js/objeto-literal.js
+++ Sección 3 Fundamentos de JavaScript, primitivos, arreglos, objetos y funciones básicas/js/objeto-literal.js
@@ -26,3 +26,27 @@ console.log('Ultimo Traje:', personaje.trajes[personaje.trajes.length - 1]);
 const x = 'vivo';
 console.log('Vivo', personaje[x]);
 console.log('Ultima pelicula', personaje[" ultima-Pelicula"]);
+
+
+
+//mas detalles
+
+delete personaje.edad; //elimina propiedad del objeto
+console.log(personaje);
+
+personaje.casado = true; //agregar
+
+const entriesPares = Object.entries(personaje);
+console.log(entriesPares);
+
+console.log(personaje);
+
+Object.freeze(personaje); //bloquea el objeto no me deja agregar mas nada al objeto
+personaje.dinero = 10000000;
+personaje.direccion.ubicacion = 'Panama';
+console.log(personaje);
+
+const propiedades = Object.getOwnPropertyNames(personaje);
+const valores = Object.values(personaje);
+
+console.log(propiedades, valores);
