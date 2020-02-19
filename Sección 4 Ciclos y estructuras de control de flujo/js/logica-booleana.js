62b5e5fa2f8e7d3eb7458d5b6508ced8fd013aa8
jguerrero101314 <joelg1014@hotmail.com>
2020-02-19 11:35:37 -0500

    36. Pro tip: Asignaciones con operadores

--- Sección 4 Ciclos y estructuras de control de flujo/js/logica-booleana.js
+++ Sección 4 Ciclos y estructuras de control de flujo/js/logica-booleana.js
@@ -38,3 +38,16 @@ console.log('4 condiciones', true || true || true || false);
 
 
 console.warn('Asignaciones');
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+
+const a1 = true && 'Hola Mundo';
+const a2 = 'Hola' && 'Mundo' && soyFalso;
+const a3 = soyFalso || 'Ya no soy falso';
+
+const a4 = soyFalso || soyUndefined || soyNull || 'Ya no soy falso de nuevo';
+const a5 = soyFalso || soyUndefined || regresaTrue() || 'Ya no soy falso de nuevo';
+
+console.log({ a1, a2, a3, a4 });
