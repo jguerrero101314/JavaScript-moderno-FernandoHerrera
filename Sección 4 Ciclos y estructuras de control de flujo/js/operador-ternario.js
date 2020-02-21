8f0ad2aeacfd5117c8ee8ed2504911e2cec3b51d
jguerrero101314 <joelg1014@hotmail.com>
2020-02-21 09:00:36 -0500

    37. Operador condicional ternario

--- Sección 4 Ciclos y estructuras de control de flujo/js/operador-ternario.js
+++ Sección 4 Ciclos y estructuras de control de flujo/js/operador-ternario.js
@@ -1,38 +1,42 @@
-const dia = 0;
-const horaActual = 10;
+const dia = 1;
+const horaActual = 8;
 
 let horaApertura, mensaje;
 
-if (dia === 0 || dia === 6) {
+// if (dia === 0 || dia === 6) {
 
-    console.log('FIn de semana');
-    horaApertura = 9;
-} else {
-    console.log('dia de semana');
-    horaApertura = 11;
-}
+//     console.log('FIn de semana');
+//     horaApertura = 9;
+// } else {
+//     console.log('dia de semana');
+//     horaApertura = 11;
+// }
 
-if (horaActual >= horaApertura) {
-    mensaje = 'esta abierto';
-} else {
-    mensaje = `Esta cerrado, hoy abrimos a las ${horaApertura} `;
-}
-console.log({ horaApertura, mensaje });
-
-
+// if (horaActual >= horaApertura) {
+//     mensaje = 'esta abierto';
+// } else {
+//     mensaje = `Esta cerrado, hoy abrimos a las ${horaApertura} `;
+// }
+// console.log({ horaApertura, mensaje });
 
 
-if ([0, 6].includes(dia)) {
-    console.log('FIn de semana');
-    horaApertura = 9;
-} else {
-    console.log('dia de semana');
-    horaApertura = 11;
-}
 
-if (horaActual >= horaApertura) {
-    mensaje = 'esta abierto';
-} else {
-    mensaje = `Esta cerrado, hoy abrimos a las ${horaApertura} `;
-}
+
+// if ([0, 6].includes(dia)) {
+//     console.log('FIn de semana');
+//     horaApertura = 9;
+// } else {
+//     console.log('dia de semana');
+//     horaApertura = 11;
+// }
+
+// if (horaActual >= horaApertura) {
+//     mensaje = 'esta abierto';
+// } else {
+//     mensaje = `Esta cerrado, hoy abrimos a las ${horaApertura} `;
+// }
+// console.log({ horaApertura, mensaje });
+
+horaApertura = ([0, 6].includes(dia)) ? 9 : 11;
+mensaje = (horaActual >= horaApertura) ? 'esta abierto' : `Esta cerrado, hoy abrimos a las ${horaApertura}`;
 console.log({ horaApertura, mensaje });
