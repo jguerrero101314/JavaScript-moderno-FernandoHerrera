08dc482434f4421b52e7d4da53da9e82f2d5fc5b
jguerrero101314 <joelg1014@hotmail.com>
2020-02-27 10:43:04 -0500

    72. Métodos en las clases

--- Sección 7 Clases en JavaScript y ESNext private properties/assets/js/classes/class.js
+++ Sección 7 Clases en JavaScript y ESNext private properties/assets/js/classes/class.js
@@ -1,3 +1,19 @@
+// class Persona {
+//     nombre = '';
+//     codigo = '';
+//     frase = '';
+//     constructor(nombre, codigo, frase) {
+//         this.nombre = nombre;
+//         this.codigo = codigo;
+//         this.frase = frase;
+//     }
+// }
+// const spiderman = new Persona('Peter Parker', 'Spider', 'Soy tu amigable vecino Spiderman');
+// const ironman = new Persona('Tony Stark', 'Iron man', 'yo soy iron man');
+// console.log(spiderman);
+// console.log(ironman);
+
+
 class Persona {
     nombre = '';
     codigo = '';
@@ -7,8 +23,19 @@ class Persona {
         this.codigo = codigo;
         this.frase = frase;
     }
+    quienSoy() {
+        console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`)
+    }
+    miFrase() {
+        this.quienSoy();
+        console.log(`${this.codigo} dice: ${this.frase}`);
+    }
 }
 const spiderman = new Persona('Peter Parker', 'Spider', 'Soy tu amigable vecino Spiderman');
 const ironman = new Persona('Tony Stark', 'Iron man', 'yo soy iron man');
 console.log(spiderman);
 console.log(ironman);
+spiderman.quienSoy();
+spiderman.miFrase();
+ironman.quienSoy();
+ironman.miFrase();
