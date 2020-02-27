2dc21a23968ec4a5f02ea61b07f76272d4b740bf
jguerrero101314 <joelg1014@hotmail.com>
2020-02-27 11:00:51 -0500

    73. Sets y Gets

--- Sección 7 Clases en JavaScript y ESNext private properties/assets/js/classes/class.js
+++ Sección 7 Clases en JavaScript y ESNext private properties/assets/js/classes/class.js
@@ -14,14 +14,46 @@
 // console.log(ironman);
 
 
+// class Persona {
+//     nombre = '';
+//     codigo = '';
+//     frase = '';
+//     constructor(nombre, codigo, frase) {
+//         this.nombre = nombre;
+//         this.codigo = codigo;
+//         this.frase = frase;
+//     }
+//     quienSoy() {
+//         console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`)
+//     }
+//     miFrase() {
+//         this.quienSoy();
+//         console.log(`${this.codigo} dice: ${this.frase}`);
+//     }
+// }
+
+// console.log(spiderman);
+// console.log(ironman);
+// spiderman.quienSoy();
+// spiderman.miFrase();
+// ironman.quienSoy();
+// ironman.miFrase();
+
 class Persona {
     nombre = '';
     codigo = '';
     frase = '';
+    comida = '';
     constructor(nombre, codigo, frase) {
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+    }
+    set setComidaFavorita(comida) {
+        this.comida = comida.toUpperCase();
+    }
+    get getComidaFavorita() {
+        return `La comida favorita de ${this.nombre} es ${this.comida}`;
     }
     quienSoy() {
         console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`)
@@ -31,10 +63,10 @@ class Persona {
         console.log(`${this.codigo} dice: ${this.frase}`);
     }
 }
-
-console.log(spiderman);
-console.log(ironman);
+const spiderman = new Persona('Peter Parker', 'Spider', 'Soy tu amigable vecino Spiderman');
 spiderman.quienSoy();
-spiderman.miFrase();
-ironman.quienSoy();
-ironman.miFrase();
+spiderman.setComidaFavorita = 'El pie de cereza de la tia may';
+
+spiderman.nemesis = 'Duende verde';
+
+console.log(spiderman.getComidaFavorita);
