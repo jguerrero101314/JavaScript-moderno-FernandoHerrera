78a20b6cacba447f7ef8c130277a46b8b3878fda
jguerrero101314 <joelg1014@hotmail.com>
2020-02-28 09:19:51 -0500

    74. Propiedades, gets y métodos estáticos

--- Sección 7 Clases en JavaScript y ESNext private properties/assets/js/classes/class.js
+++ Sección 7 Clases en JavaScript y ESNext private properties/assets/js/classes/class.js
@@ -39,7 +39,48 @@
 // ironman.quienSoy();
 // ironman.miFrase();
 
+// class Persona {
+//     nombre = '';
+//     codigo = '';
+//     frase = '';
+//     comida = '';
+//     constructor(nombre, codigo, frase) {
+//         this.nombre = nombre;
+//         this.codigo = codigo;
+//         this.frase = frase;
+//     }
+//     set setComidaFavorita(comida) {
+//         this.comida = comida.toUpperCase();
+//     }
+//     get getComidaFavorita() {
+//         return `La comida favorita de ${this.nombre} es ${this.comida}`;
+//     }
+//     quienSoy() {
+//         console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}`)
+//     }
+//     miFrase() {
+//         this.quienSoy();
+//         console.log(`${this.codigo} dice: ${this.frase}`);
+//     }
+// }
+// const spiderman = new Persona('Peter Parker', 'Spider', 'Soy tu amigable vecino Spiderman');
+// spiderman.quienSoy();
+// spiderman.setComidaFavorita = 'El pie de cereza de la tia may';
+
+// spiderman.nemesis = 'Duende verde';
+
+// console.log(spiderman.getComidaFavorita);
+
 class Persona {
+
+    static _conteo = 0;
+    static get conteo() {
+        return Persona._conteo + ' instancias';
+    }
+    static mensaje() {
+        console.log(this.nombre);
+        console.log('soy un metodo estatico');
+    }
     nombre = '';
     codigo = '';
     frase = '';
@@ -48,6 +89,8 @@ class Persona {
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+
+        Persona._conteo++;
     }
     set setComidaFavorita(comida) {
         this.comida = comida.toUpperCase();
@@ -64,9 +107,19 @@ class Persona {
     }
 }
 const spiderman = new Persona('Peter Parker', 'Spider', 'Soy tu amigable vecino Spiderman');
+
 spiderman.quienSoy();
 spiderman.setComidaFavorita = 'El pie de cereza de la tia may';
 
 spiderman.nemesis = 'Duende verde';
 
+
 console.log(spiderman.getComidaFavorita);
+// Persona._conteo = 2;
+console.log('conteo estatico', Persona._conteo);
+console.log(Persona.conteo);
+Persona.mensaje();
+
+Persona.prpiedadExterna = 'Hola Mundo';
+console.log(Persona.prpiedadExterna);
+console.log(Persona);
