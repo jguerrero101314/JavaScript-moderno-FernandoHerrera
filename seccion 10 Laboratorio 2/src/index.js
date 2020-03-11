d7dbb0f02708eab33d496d4ed5d93b4986d14dd2
jguerrero101314 <joelg1014@hotmail.com>
2020-03-11 10:49:03 -0500

    107. Construir las tareas en el HTML

--- seccion 10 Laboratorio 2/src/index.js
+++ seccion 10 Laboratorio 2/src/index.js
@@ -1,10 +1,13 @@
 import './styles.css';
 import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 
 const todoList = new TodoList();
 const tarea = new Todo('aprender js');
 
 
+
 todoList.nuevoTodo(tarea);
 console.log(todoList);
+crearTodoHtml(tarea);
