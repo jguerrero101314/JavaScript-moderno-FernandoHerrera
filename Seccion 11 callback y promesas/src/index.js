63c2bba6df95e023131ba17f365151d7f47c8a16
jguerrero101314 <joelg1014@hotmail.com>
2020-03-14 11:41:26 -0500

    123. Callback Hell

--- Seccion 11 callback y promesas/src/index.js
+++ Seccion 11 callback y promesas/src/index.js
@@ -1,12 +1,18 @@
 import './styles.css';
 import { buscarHeroe } from './js/callbacks';
 
-const heroeId = 'capi';
-buscarHeroe(heroeId, (err, heroe) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(heroe);
-    }
+const heroeId1 = 'capi';
+const heroeId2 = 'spider';
 
+buscarHeroe(heroeId1, (err, heroe1) => {
+    // if (err) {
+    //     console.error(err);
+    // } else {
+    //     console.log(heroe);
+    // }
+    if (err) { return console.error(err); }
+    buscarHeroe(heroeId2, (err, heroe2) => {
+        if (err) { return console.error(err); }
+        console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision`);
+    });
 });
