d7330816999560e61e92ae5a185e5616e84ef0bf
jguerrero101314 <joelg1014@hotmail.com>
2020-03-15 12:43:00 -0500

    126. Promise.catch

--- Seccion 11 callback y promesas/src/index.js
+++ Seccion 11 callback y promesas/src/index.js
@@ -1,42 +1,8 @@
 import './styles.css';
-import { buscarHeroe as buscarHeroeCallback } from './js/callbacks';
-import { buscarHeroe } from './js/promise';
+import { promesaLenta, promesaMedia, promesaRapida } from './js/promise';
 
-
-const heroeId1 = 'capi';
-const heroeId2 = 'spider';
-const heroeId3 = 'iron';
-
-
-/* callbacks
-buscarHeroe(heroeId1, (err, heroe1) => {
-    // if (err) {
-    //     console.error(err);
-    // } else {
-    //     console.log(heroe);
-    // }
-    if (err) { return console.error(err); }
-    buscarHeroe(heroeId2, (err, heroe2) => {
-        if (err) { return console.error(err); }
-        console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision`);
-    });
-});*/
-//promesas
-/*
-buscarHeroe(heroeId1).then(heroe1 => {
-    // console.log(`enviando a ${heroe.nombre} a la mision`);
-    buscarHeroe(heroeId2).then(heroe2 => {
-        // console.log(`enviando a ${heroe.nombre} a la mision`);
-        console.log(`Enviando a ${heroe1.nombre} y ${heroe2.nombre} a la mision`);
-    });
-});
-*/
-//promise all
-Promise.all([buscarHeroe(heroeId1), buscarHeroe(heroeId2), buscarHeroe(heroeId3)])
-    .then(([heroe1, heroe2, heroe3]) => {
-        // console.log('Promise.all', heroes);
-        console.log(`Enviando a ${heroe1.nombre} , ${heroe2.nombre} y  ${heroe3.nombre} a la mision`);
-
-    });
-
-console.log('Fin de programa');
+// promesaLenta.then(console.log);
+// promesaMedia.then(console.log);
+// promesaRapida.then(console.log);
+Promise.race([promesaLenta, promesaMedia, promesaRapida])
+    .then(console.log);
