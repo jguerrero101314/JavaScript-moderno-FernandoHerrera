e80456e0b063216de3d80a0f47dd1b387ba60be5
jguerrero101314 <joelg1014@hotmail.com>
2020-03-17 13:05:44 -0500

    138. Pro tip: Promesas en cadena

--- Seccion 12 peticiones http y subida de archivos/src/index.js
+++ Seccion 12 peticiones http y subida de archivos/src/index.js
@@ -1,10 +1,12 @@
 const jokeUrl = 'https://api.chucknorris.io/jokes/random';
-fetch(jokeUrl).then(resp => {
-    resp.json().then(({ id, value }) => {
-        console.log(id);
-        console.log(value);
-
-
-
+// fetch(jokeUrl).then(resp => {
+//     resp.json().then(({ id, value }) => {
+//         console.log(id);
+//         console.log(value);
+//     });
+// });
+fetch(jokeUrl)
+    .then(resp => resp.json())
+    .then(({ id, value }) => {
+        console.log(id, value);
     });
-})
