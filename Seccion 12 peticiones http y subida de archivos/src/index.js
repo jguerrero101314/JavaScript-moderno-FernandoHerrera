a6a7627f5051378cd7dabf792630136cfb147581
jguerrero101314 <joelg1014@hotmail.com>
2020-03-23 11:30:16 -0500

    143. CRUD - Leer usuarios

--- Seccion 12 peticiones http y subida de archivos/src/index.js
+++ Seccion 12 peticiones http y subida de archivos/src/index.js
@@ -1,9 +1,12 @@
 // import { init } from "./js/chistes.page";
 // init();
 
-import { obtenerUsuarios } from "./js/http-provider";
-import { init } from "./js/usuarios-page";
+// import { obtenerUsuarios } from "./js/http-provider";
+// import { init } from "./js/usuarios-page";
 
 
-obtenerUsuarios().then(console.log);
-init();
+// obtenerUsuarios().then(console.log);
+// init();
+import * as CRUD from './js/crud-provider';
+
+CRUD.getUsuario(2).then(console.log);
